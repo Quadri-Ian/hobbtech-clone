1417cc7ee7c5d111d0f36ab066f779771ab4d33d
Quadri-Ian <104133443+Quadri-Ian@users.noreply.github.com>
2022-11-09 17:43:26 +0100

    first commit

---
+++ faq.js
@@ -0,0 +1,53 @@
+function myFunction(event){
+    var x = document.querySelector(".mydiv");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }else {
+        x.style.display = "none";
+    }
+}
+
+function myFunction2(event){
+    var b = document.querySelector(".mydiv1");
+    if (b.style.display === "none") {
+        b.style.display = "block";
+    }else {
+        b.style.display = "none";
+    }
+}
+
+function myFunction3(event){
+    var b = document.querySelector(".mydiv2");
+    if (b.style.display === "none") {
+        b.style.display = "block";
+    }else {
+        b.style.display = "none";
+    }
+}
+
+function myFunction4(event){
+    var b = document.querySelector(".mydiv3");
+    if (b.style.display === "none") {
+        b.style.display = "block";
+    }else {
+        b.style.display = "none";
+    }
+}
+
+function myFunction5(event){
+    var b = document.querySelector(".mydiv4");
+    if (b.style.display === "none") {
+        b.style.display = "block";
+    }else {
+        b.style.display = "none";
+    }
+}
+
+function myFunction6(event){
+    var b = document.querySelector(".mydiv5");
+    if (b.style.display === "none") {
+        b.style.display = "block";
+    }else {
+        b.style.display = "none";
+    }
+}
